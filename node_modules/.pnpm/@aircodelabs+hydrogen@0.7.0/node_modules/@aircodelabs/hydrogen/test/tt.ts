2539bf948147b14d5105686b76416a2438c6a597
allmors <jesmora@protonmail.com>
2024-09-26 02:09:32 +0000

    up

--- node_modules/.pnpm/@aircodelabs+hydrogen@0.7.0/node_modules/@aircodelabs/hydrogen/test/tt.ts
+++
@@ -1,9 +0,0 @@
-// @see https://docs.aircode.io/guide/functions/
-import aircode from 'aircode';
-
-export default async function (params: any, context: any) {
-  console.log('Received params:', params);
-  return {
-    message: 'Hi, AirCode.',
-  };
-};
